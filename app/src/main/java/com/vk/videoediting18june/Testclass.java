aebb37f81a3d88de524d400a755f060b6ddb68f3
DanJohan <bestbuysystems01@gmail.com>
2018-06-18 16:25:13 +0530

    activity_main.xml : Changes in button MainActivity.java : Handling of button

--- app/src/main/java/com/vk/videoediting18june/Testclass.java
+++
@@ -1,9 +0,0 @@
-package com.vk.videoediting18june;
-
-/**
- * Created by jwick on 6/18/18.
- */
-
-public class Testclass {
-
-}
--- app/src/main/java/com/vk/videoediting18june/mycloass.java
+++
@@ -1,4 +0,0 @@
-package com.vk.videoediting18june;
-
-public class mycloass {
-}
